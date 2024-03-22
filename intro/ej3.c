e8101a10955b5abbbd2134770d6f8bac56abb32b
Gonzalo Javier Gallardo <65740928+gonzagallard@users.noreply.github.com>
2024-03-22 18:42:46 -0300

    función definida por ej

--- intro/ej3.c
+++ intro/ej3.c
@@ -9,18 +9,17 @@
 
 float in_data(void);
 bool data_invalid(float data);
+float celcius_to_Fahrenheit(float celcius);
 
 
 int main(void){
-    float celcius, farenh;
-    celcius = farenh = 0;
+    float celcius = 0;
 
     printf("ingrese la cantidad de C a convertir:\t");
     if(data_invalid(celcius = in_data()))
         return EXIT_FAIL;
-
-    farenh = (float)9/5*celcius + 32;
-    printf("%.3f C son %.3f F\nFIN PROGRAMA\n",celcius, farenh);
+ 
+    printf("%.3f C son %.3f F\nFIN PROGRAMA\n",celcius, celcius_to_Fahrenheit(celcius));
 
     return EXIT_SUCCESS;
 }
@@ -39,3 +38,6 @@ bool data_invalid(float data){
     return false;
 }
 
+float celcius_to_Fahrenheit(float celcius){
+ return (float)9/5*celcius + 32;
+}
